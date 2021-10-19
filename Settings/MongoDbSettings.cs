518bbfffbc3f1fada2578a354c258f49f59610ee
linvmuer <linvle2@gmail.com>
2021-10-19 11:57:31 +0200

    Added Health Checks and Packages

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+    
+    
+}
--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
+using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +17,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 namespace Catalog
 {
@@ -28,12 +37,33 @@ namespace Catalog
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddSingleton<IItemsRepository, InMemItemsRepository>(); // One instance through the lifetime of the application
-            services.AddControllers();
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            services.AddSingleton<IMongoClient>(serviceProvider =>
+            {
+                // var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                 Console.WriteLine($"Password is {mongoDbSettings.Password} Port is {mongoDbSettings.Port} ");
+                return new MongoClient(mongoDbSettings.ConnectionString);
+            });
+            services.AddSingleton<IItemsRepository, MongoDbItemsRepository>(); // One instance through the lifetime of the application
+            // To fix the issue where GetItemAsync is not being accepted add the following code to the 
+            // service add Controller like so
+            // the default behavior is to remove the async at runtime thats why when l removed it it worked
+            services.AddControllers(
+                options=>
+                    options.SuppressAsyncSuffixInActionNames=false);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                .AddMongoDb(
+                    mongoDbSettings.ConnectionString,
+                    name:"mongodbb",
+                    timeout:TimeSpan.FromSeconds(3),
+                    tags:new[]{"ready"});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +85,35 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                
+                endpoints.MapHealthChecks("/health/ready",
+                    new HealthCheckOptions
+                    {
+                        Predicate = (check)=>check.Tags.Contains("ready"),
+                        ResponseWriter = async (context, report) =>
+                        {
+                            var result = JsonSerializer.Serialize(
+                                new
+                                {
+                                    status=report.Status.ToString(),
+                                    checks=report.Entries.Select(entry =>
+                                   new  {
+                                        name=entry.Key,
+                                        status=entry.Value.Status.ToString(),
+                                        exception=entry.Value.Exception !=null?entry.Value.Exception.Message:"None",
+                                        duration=entry.Value.Duration.ToString()
+                                    })
+                                });
+                            context.Response.ContentType = MediaTypeNames.Application.Json;
+                            await context.Response.WriteAsync(result);
+                        }
+                    });
+                endpoints.MapHealthChecks("/health/live",
+                    new HealthCheckOptions
+                    {
+                        Predicate = (_)=>false
+                    });
+                
             });
         }
     }
