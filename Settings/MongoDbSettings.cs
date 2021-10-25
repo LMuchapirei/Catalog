632abaf7beb5035fa3f4a05abe478be7c5f0688d
linvmuer <linvle2@gmail.com>
2021-10-25 11:50:32 +0200

    Setup mongodb settings

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -6,6 +6,7 @@
         public int Port { get; set; }
         public string User { get; set; }
         public string Password { get; set; }
+        // public string ConnectionString => $"mongodb://{User}:{Password}@{Host}:{Port}";
         public string ConnectionString => $"mongodb://{Host}:{Port}";
     }
     
